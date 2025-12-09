9efd651de6697b585473b45ef97f0fd50f44c184
Harley062 <harleygteixeira@gmail.com>
2025-12-09 11:48:50 -0300

    Adiciona novas imagens e atualiza a função de exportar nota para incluir novos passos no processo

--- pipeline/exportar_nota.py
+++ pipeline/exportar_nota.py
@@ -6,4 +6,22 @@ import pyautogui
 def exportar_nota():
     click_on_image('exportar_xml/EpuPRI53T2.png', confidence=0.7, timeout=20)
     
+    time.sleep(5)
+    
+    click_on_image('exportar_xml/todos_os_modulos.png', confidence=0.7, timeout=20)
+    
+    time.sleep(5)
+    
+    click_on_image('exportar_xml/fiscal.png', confidence=0.7, timeout=20)
+    
+    time.sleep(30)
+    
+    click_on_image('exportar_xml/fechar.png', confidence=0.7, timeout=20)
+    
+    time.sleep(5)
+    
+    click_on_image('exportar_xml/gerenciador_de_nf.png', confidence=0.7, timeout=20)
+    
+    
+    
     return True
